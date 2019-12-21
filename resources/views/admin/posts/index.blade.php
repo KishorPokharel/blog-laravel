ca9ead69b67c82ce15fd929e93e6c95b17a5ab38
KishorPokharel <fool.fohyuh123@gmail.com>
2019-12-21 21:57:30 +0545

    first commit

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">Posts</div>
+
+        <div class="card-body">
+            
+        </div>
+    </div>
+@endsection
